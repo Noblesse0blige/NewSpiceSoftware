a87276aae183a78e29339be109680bfaa20c0403
Noblesse0blige <lenorekoenig@gmail.com>
2017-05-21 16:52:25 -0400

    Add project files.

---
+++ NewSpiceSoftware/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewSpiceSoftware.Startup))]
+namespace NewSpiceSoftware
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
